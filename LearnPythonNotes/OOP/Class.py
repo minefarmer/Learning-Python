97a521737c67f243e5caf56b1ce2f1eb8df84cbb
minefarmer <pgoldfarm@yahoo.com>
2020-08-02 20:56:48 -0500

    Good Night!

--- LearnPythonNotes/OOP/Class.py
+++ LearnPythonNotes/OOP/Class.py
@@ -6,31 +6,33 @@ A class consists of variables(Attributes) and functions (Methods)
 
 __init__() is a built-in function. Also known as the initializer. Used to initialize objects with initial values. All classes should have them. It is called automatically when a new object is created from a class.
 
+Modifying a Class
+Add new attributes
+Modify existing attribute values
+Delete attributes
 
 
+Importing Classes
+Once I have created a class you can also reuse the code you have created for that class, I can use it in other programs that you're writing by importing the classes
+I can import them into another program that I are working on == just like importing a module
 
 
+Class Vs Instance (Object) Variables
 
-
-
-
-
-
-
-
-
-
-
+Class Variables                                 Instance (Object) Variables
+Defined outside any method                      Defined inside class methods
+Accessed outside with className                 Accessed outside class with object name
+Not Prefixed                                    Prefixed with self keyword
+Modifications affwcts all classes instances     Modifications to object local
+Classes indented                                Instances are not indented
 
 """
-# Instantiating a class   56
-# Accessing Methods   63
-# 
-# 
-# 
-# 
-# 
-# 
+# Instantiating a class   58
+# Accessing Methods   65
+# Accessing Atribute   71
+# Modifying a Class   79
+# Change the value of an attribute   90
+# Importing classes   108
 # 
 # 
 # 
@@ -40,8 +42,9 @@ __init__() is a built-in function. Also known as the initializer. Used to initia
 class Instructors:  # all classes should have the uderscore underscore in it, as shown below.
     companyName = "BlueLime"   # this is a variable
     
-    def __init__(self,course):  # this is a method   **** this method __init__ is a built in function == it is called the initializer, The initial value can be changed later.
+    def __init__(self,course, duration):  # this is a method   **** this method __init__ is a built in function == it is called the initializer, The initial value can be changed later.
         self.course = course  # this is a variable
+        self.duration = duration
         # The self paremeter is basically a reference to the current instance of the class
     
     def printinfo(self):  # this is a method
@@ -53,9 +56,9 @@ class Instructors:  # all classes should have the uderscore underscore in it, as
 
 
 # Instantiating a class
-elearning = Instructors('Python for Beginners')  # on line 45 [course] refers to 'Python for Beginners' here.
+elearning = Instructors('Python for Beginners',7)  # on line 45 [course] refers to 'Python for Beginners' here.
 
-bls = Instructors('Django for beginners')  # on line 45 [course] refers to 'Django for Beginners' here.
+bls = Instructors('Django for beginners',8)  # on line 45 [course] refers to 'Django for Beginners' here.
 
 
 
@@ -63,3 +66,43 @@ bls = Instructors('Django for beginners')  # on line 45 [course] refers to 'Djan
 # ObjectName.Method  ** so I can call the method on a new object I have instantiated.
 
 bls.printinfo()  # My Company name is  BlueLime  *** calls method on line 47
+
+
+
+# Accessing Atribute
+# ObjectName.Attribute
+
+print(bls.course)  # My Company name is  BlueLime
+                    # Django for beginners
+                    
+                    
+                    
+# Modifying a Class
+# Adding a new attribute
+# Changes made to the init   44
+    # def __init__(self,course, duration):  # (duration) added
+    #     self.duration = duration  # this line added
+    # changes on 58, 7:  60, 8
+
+print(bls.duration)   # 8   ***  from line 60
+
+
+
+# Change the value of an attribute   ***  bls on line 60
+
+bls.course = "HTML"
+
+print(bls.course)    # HTML
+
+
+# Delete an attribute
+
+del bls.duration
+
+print(bls.duration)  # Traceback (most recent call last):
+                    # File "/home/carl/Github/Learning-Python/LearnPythonNotes/OOP/Class.py", line 101, in <module>
+                    #     print(bls.duration)
+                    # AttributeError: 'Instructors' object has no attribute 'duration'
+                    
+                    
+
---
+++ LearnPythonNotes/OOP/Inheritance.py
@@ -0,0 +1,78 @@
+"""INHERITANCE
+Inheritance is basically the process that allows us to create a new class and reusing the code from the existing parent class
+The parent class (superclass or base class) is the class being inherited from
+The child class (subclass or derived) is the class that inherits from another class
+
+If I create a child class without an __init__()method it inherits all methods and attributes of the parent class
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+# Basic class   31
+# Creating a child class   45
+# Adding an __init__ to a child class   53
+# Adding a call to the parent __init__ method   67
+# 
+# 
+# 
+# 
+# 
+
+# Basic class
+class Person:
+    def __init__(self,fname,lname):
+        self.firstname  = fname
+        self.lastname = lname
+        
+    def printname(self):
+        print(self.firstname,self.lastname)
+        
+florist = Person('Jane', 'Flowers')  # I've instanciated the class called Person
+florist.printname()  # Jane Flowers
+
+
+
+# Creating a child class
+class Lawyers(Person):  # Inheriting the methods and variables from the parent class
+    pass
+happy_lawyers = Lawyers('Jack','Smiley')  # this will inherit the methods and variables of the parent class
+happy_lawyers.printname()  # Jack Smiley
+
+
+
+# Adding an __init__ to a child class
+class Lawyers(Person):
+    def __init__(self,fname,lname):  # This overrides th init of the parent function
+        self.firstname  = fname
+        self.lastname = lname
+        
+    def printinfo(self):
+        print(self.firstname,self.lastname)
+        
+happy_lawyers = Lawyers('Jack','Smiley')
+happy_lawyers.printinfo()  # Jack Smiley
+
+
+
+#  adding a call to the parent __init__ method
+class Lawyers(Person):
+    def __init__(self,fname,lname):
+        Person.__init__(self,fname,lname)
+        # self.firstname  = fname
+        # self.lastname = lname
+        
+    def printinfo(self):
+        print(self.firstname,self.lastname)
+        
+happy_lawyers = Lawyers('Jack','Smiley')
+happy_lawyers.printname()  # Jack Smiley
