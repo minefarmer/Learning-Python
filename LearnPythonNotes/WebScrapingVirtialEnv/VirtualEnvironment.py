61903e1c00998b239bc9868b497f95c269d5e24d
minefarmer <pgoldfarm@yahoo.com>
2020-08-09 11:59:03 -0500

    back to win 10

--- LearnPythonNotes/WebScrapingVirtialEnv/VirtualEnvironment.py
+++ LearnPythonNotes/WebScrapingVirtialEnv/VirtualEnvironment.py
@@ -4,5 +4,10 @@ Allows me to use different packages and versions for various projects. i.e. my c
 Virtualenv is the tool used to create isolated python environemts.
 
 
+INSTALL VIRTUAL ENVIROMENT
+cd into your project directory.
+pip install virtualenv.
+If it is already installed on my computer, I will be told and installation will stop
+
 
 '''
