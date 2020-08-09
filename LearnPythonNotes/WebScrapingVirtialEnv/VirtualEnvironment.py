f515264c4a3ba9fffe10937f6dc56f6cfe753ca7
minefarmer <pgoldfarm@yahoo.com>
2020-08-09 12:54:53 -0500

    back to linux

--- LearnPythonNotes/WebScrapingVirtialEnv/VirtualEnvironment.py
+++ LearnPythonNotes/WebScrapingVirtialEnv/VirtualEnvironment.py
@@ -4,5 +4,8 @@ Allows me to use different packages and versions for various projects. i.e. my c
 Virtualenv is the tool used to create isolated python environemts.
 
 
+CREATE AN ISOLATED VIRTUAL ENVERONMENT
+Make sure I'm in the correct project directory
+
 
 '''
