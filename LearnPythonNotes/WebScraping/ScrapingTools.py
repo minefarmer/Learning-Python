c09c06ec5d0eeeedf59a353c5108da61893eefe9
minefarmer <pgoldfarm@yahoo.com>
2020-08-08 20:12:08 -0500

    good night

---
+++ LearnPythonNotes/WebScraping/ScrapingTools.py
@@ -0,0 +1,10 @@
+"""TOOLS USED FOR WEB SCRAPING
+
+Scrapy is a free open source application framework. 
+Pip can be used to install Scrapy. 
+It is used for crawling web sites and extracting data.
+
+Beautiful Soup is used to extract data from HTML and XML files.
+Us pip.  pip install beautifulsoup4 (bs4)
+
+"""
---
+++ LearnPythonNotes/WebScraping/WebScraping.py
@@ -0,0 +1,22 @@
+"""wHAT IS WEB SCRAPING
+This is also referred to as web harvesting and web data extraction.
+This process of automatically downloading a web page's data and extracting information from it.
+
+
+BENEFITS OF WEB SCRAPING
+Component of applications used for web indexing i.e. people.
+Web and data mining.
+Online price monitering.
+Product review to watch competition.
+Grather real estate listing.
+Weather data monitering.
+Website change detection.
+Research.
+
+
+BASIC RULES FOR WEB SCRAPING
+ALWAYS CHECK A WEBSITE'S TERMS AND CONDITIONS , BRFORE I SCRAPE IT, TO AVOID ANY LEGAL ISSUES.
+Do not request data from a website too aggressively(spamming) with my program as this may overload and break the website.
+The website layout may change so visit occasionally to check and then adapt my code.
+
+"""
